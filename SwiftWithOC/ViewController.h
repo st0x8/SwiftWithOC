e273743f1786c5c1d8167ab36da33b677fd3fa36
st0x8 <ste0x08@gmail.com>
2016-05-24 12:47:48 +0800

    Initial Commit

---
+++ SwiftWithOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwiftWithOC
+//
+//  Created by Roy Lin on 5/24/16.
+//  Copyright © 2016 Roy Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SwiftWithOC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SwiftWithOC
+//
+//  Created by Roy Lin on 5/24/16.
+//  Copyright © 2016 Roy Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
